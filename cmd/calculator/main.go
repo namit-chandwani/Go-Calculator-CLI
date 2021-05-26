b0851526b3296a35d33e6734025240daff97a503
Namit Chandwani <namitchandwani99@gmail.com>
2021-05-26 18:15:12 +0530

    feat: add v1.0

--- cmd/calculator/main.go
+++ cmd/calculator/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/namit-chandwani/Go-Calculator-CLI/pkg/cmdloader"
+	"github.com/spf13/cobra"
+	"github.com/spf13/cobra/doc"
+)
 
 func main() {
-	fmt.Println("Welcome to the Calculator CLI!")
+	cmd := &cobra.Command{
+		Use:          "calculator",
+		Short:        "Calculator CLI",
+		Long:         `A basic calculator CLI built in Golang using the Cobra library.`,
+		SilenceUsage: true,
+	}
+
+	cmdloader.AddCommands(cmd)
+
+	err := doc.GenMarkdownTree(cmd, "./docs/commands")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := cmd.Execute(); err != nil {
+		log.Fatal(err)
+	}
 }
